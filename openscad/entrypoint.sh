9288a8adc27a188f9c0663bfb51715193b4fbef4
Kurt Hutten IrevDev <k.hutten@protonmail.ch>
2022-05-29 11:33:08 +1000

    update

--- openscad/entrypoint.sh
+++ openscad/entrypoint.sh
@@ -2,11 +2,11 @@
 
 echo "Hello $1"
 time=$(date)
-ls
+ls -lah
 
 xvfb-run --auto-servernum --server-args "-screen 0 1024x768x24" openscad-nightly -o ./output.stl -P default ./basic.scad
 
-ls
+ls -lah
 
 
 echo "::set-output name=time::$time"
